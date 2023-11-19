67613db3a9f0e02161df2506f5c55443f9be9c3d
NureniJamiu <nurenijamiu951@gmail.com>
2023-11-19 13:13:29 +0100

    fix; hydration issue

--- app/(web)/(routes)/blogs/[postId]/page.tsx
+++ app/(web)/(routes)/blogs/[postId]/page.tsx
@@ -13,7 +13,7 @@ interface Post {
     title: string;
     content: any,
     category: string,
-    image: any,
+    image: string,
     author: string,
     createdAt: string,
     updatedAt: string,
@@ -45,31 +45,33 @@ const SingleBlog = () => {
         <section className="flex justify-center items-center">
             {!isLoading ? (
                 <div className="w-full">
-                    <div className="relative h-64 bg-green-600">
-                        <Image src={post?.image} alt="" fill className="absolute object-cover object-top" />
-                        <div className="absolute bg-black top-0 left-0 right-0 w-full h-full bg-opacity-70"></div>
-                        <div className="absolute w-full h-full flex items-center justify-center text-center mb-8">
-                            <div className="text-gray-300">
-                                <h2 className=" font-bold lg:text-6xl text-center mb-2 capitalize lg:px-20">
-                                    {post?.title}
-                                </h2>
-                                <small>
-                                    {moment(post?.createdAt).format("LL")} by{" "}
-                                    <Link href="#" className="underline text-green-700">
-                                        {post?.author}
-                                    </Link>
-                                </small>
+                    {post && (
+                        <div className="relative h-64 bg-green-600">
+                            <Image src={post?.image} alt="" fill className="absolute object-cover object-top" />
+                            <div className="absolute bg-black top-0 left-0 right-0 w-full h-full bg-opacity-70"></div>
+                            <div className="absolute w-full h-full flex items-center justify-center text-center mb-8">
+                                <div className="text-gray-300">
+                                    <h2 className=" font-bold lg:text-6xl text-center mb-2 capitalize lg:px-20">
+                                        {post?.title}
+                                    </h2>
+                                    <small>
+                                        {moment(post?.createdAt).format("LL")} by{" "}
+                                        <Link href="#" className="underline text-green-700">
+                                            {post?.author}
+                                        </Link>
+                                    </small>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    )}
                     <div className="p-8 md:px-24 lg:px-[280px]">
                         <div>
                             <div dangerouslySetInnerHTML={{ __html: post?.content }}></div>
-                            <p className="mt-12">
+                            <div className="mt-12">
                                 <p>Comr. Nureni Jamiu (Penocrat),</p>
                                 <p>21st Public Relations Officer (P.R.O),</p>
                                 <p>Amatech Lasu, {"'"}23.</p>
-                            </p>
+                            </div>
                         </div>
                     </div>
                 </div>
