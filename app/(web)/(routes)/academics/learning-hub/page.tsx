de9f94de9a9a9a8a8728b7b20ff4e84519d637d5
NureniJamiu <nurenijamiu951@gmail.com>
2023-12-08 08:05:35 +0100

    add more routes

---
+++ app/(web)/(routes)/academics/learning-hub/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+    return <div>Learning Hub</div>;
+};
+
+export default Page;
---
+++ app/(web)/(routes)/students/first-class-graduates/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+    return <div>First Class Students</div>;
+};
+
+export default Page;
