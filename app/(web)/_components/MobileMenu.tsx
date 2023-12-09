a84157d0702c869b6373c3a89f0cae1825d2ee30
NureniJamiu <nurenijamiu951@gmail.com>
2023-12-09 08:14:21 +0100

    add mobile menu links

--- app/(web)/_components/MobileMenu.tsx
+++ app/(web)/_components/MobileMenu.tsx
@@ -9,6 +9,14 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 import { Menu } from "lucide-react";
 // import { navLinks } from "../_mock";
 import Link from "next/link";
@@ -19,6 +27,8 @@ const navLinks = [
   { title: "Test", url: "test" },
   { title: "Test1", url: "test1" },
 ]
+
+const listStyle = `py-2 hover:text-blue-500 hover:underline`
 
 const MobileMenu = () => {
   const { isSignedIn } = useUser()
@@ -32,7 +42,7 @@ const MobileMenu = () => {
           <SheetTitle></SheetTitle>
           <SheetDescription className=""></SheetDescription>
         </SheetHeader>
-        <div>
+        {/* <div>
           {navLinks.map(({ title, url }) => (
             <p key={title} className="py-2 text-lg ">
               <Link href={url} className="capitalize hover:text-green-700">
@@ -40,16 +50,43 @@ const MobileMenu = () => {
               </Link>
             </p>
           ))}
-        </div>
+        </div> */}
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Students</AccordionTrigger>
+            <AccordionContent>
+              <ul>
+                <li className={listStyle}><a href="/students/executive-members">Executives</a></li>
+                <li className={listStyle}><a href="/students/legislative-members">Legislatives</a></li>
+                <li className={listStyle}><a href="/students/first-class-graduates">First Class Graduates</a></li>
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Academics</AccordionTrigger>
+            <AccordionContent>
+              <ul>
+                <li className={listStyle}><a href="/academics/learning-hub">Learning HUB</a></li>
+                <li className={listStyle}><a href="/academics/all-offered-courses">All Courses</a></li>
+                <li className={listStyle}><a href="/academics/cgpa-calculator">CGPA Calculator</a></li>
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <ul className="font-medium">
+          <li className={`${listStyle} py-4 border-b`}><a href="/lecturers"><a></a>Lecturers</a></li>
+          <li className={`${listStyle} py-4 border-b`}><a href="/blogs">Blogs</a></li>
+        </ul>
+
 
         <div className="flex flex-col mt-3 gap-2">
           <Link href="/vote">
-            <Button className="border-2 rounded border-green-600 text-green-700 hover:bg-green-600 hover:text-white">
+            <Button className="w-full border-2 rounded border-green-600 text-green-700 hover:bg-green-600 hover:text-white">
               Voting System
             </Button>
           </Link>
           <Link href={isSignedIn ? "/dashboard" : "/sign-in"}>
-            <Button className="btn-gradient rounded">{isSignedIn ? "Dashboard" : "Admin🔒"}</Button>
+            <Button className="btn-gradient rounded w-full">{isSignedIn ? "Dashboard" : "Admin🔒"}</Button>
           </Link>
         </div>
       </SheetContent>
