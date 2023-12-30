fa72a2befee14b1995556533f1be429342f5532e
NureniJamiu <nurenijamiu951@gmail.com>
2023-12-30 18:41:33 +0100

    add design title

--- app/(web)/(routes)/academics/cgpa-calculator/page.tsx
+++ app/(web)/(routes)/academics/cgpa-calculator/page.tsx
@@ -8,15 +8,16 @@ const Page = () => {
             <TitleHero title="CGPA Calculator" />
             <div className="px-3 md:px-12 lg:px-24">
                 <div className="my-5">
-                    <div className="my-5">
-                        <ul className="hidden lg:flex lg:items-center lg:justify-center gap-3">
-                            <li><b>C.U :</b> Credit Units ||</li>
-                            <li><b>T.C.U :</b> Total Credit Units ||</li>
-                            <li><b>G.P : </b>Grade Points ||</li>
-                            <li><b>T.G.P :</b> Total Grade Points ||</li>
-                            <li><b>Q.P : </b>Quality Points ||</li>
-                            <li><b>T.Q.P :</b> Total Quality Points</li>
-                        </ul>
+                    <div className="text-center px-8 md:px-24 py-8">
+                        <h2 className="text-3xl">
+                            Calculate CGPA{" "}
+                            <span className="text-[#227e5f] font-semibold">
+                                With Ease
+                            </span>
+                        </h2>
+                        <span className="italic">
+                            Plan effectively ahead for the next semester
+                        </span>
                     </div>
                     <div className="bg-slate-200 rounded min-h-[120px] p-5">
                         <CgpaCalculatorForm />
