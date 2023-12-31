c01f5bca2b2785f8519845adbd6d0401cc18b9de
NureniJamiu <nurenijamiu951@gmail.com>
2023-12-31 09:16:16 +0100

    create admission requirement page

---
+++ app/(web)/(routes)/academics/admission-requirement/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import TitleHero from "@/app/(web)/_components/TitleHero";
+
+const Page = () => {
+    return <section>
+        <TitleHero title="Admission Requirement" />
+    </section>;
+};
+
+export default Page;
